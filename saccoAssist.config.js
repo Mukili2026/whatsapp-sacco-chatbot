4e3b9909db2e87708a2bf45b0f0d1808dc46ae7c
Geoffrey Obia <postmateug@gmail.com>
2026-02-06 16:13:03 +0300

    Create saccoAssist.config.js

---
+++ saccoAssist.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  SACCO_ASSIST_BASE_URL: process.env.SACCO_ASSIST_BASE_URL || "https://api.sacco-assist.com/v1",
+  API_TOKEN: process.env.SACCO_ASSIST_API_TOKEN || "your-api-token-here"
+};
