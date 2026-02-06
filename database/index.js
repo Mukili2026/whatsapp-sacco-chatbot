0fb09857c5aa1ceb87e765e8921c4e61facd4bc2
Geoffrey Obia <postmateug@gmail.com>
2026-02-06 13:55:46 +0300

    Remove database initialization logic

--- database/index.js
+++ database/index.js
@@ -1,11 +1,1 @@
-let db;
 
-if (process.env.DB_TYPE === "postgres") {
-  db = require("./postgres");
-  console.log("🐘 Using PostgreSQL database");
-} else {
-  db = require("./sqlite");
-  console.log("📦 Using SQLite database");
-}
-
-module.exports = db;
